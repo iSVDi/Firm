a3811c0e783277946aeebbec3aad04ca1532a192
Daniil-Shestopalov <d.v.shestopalov@mail.ru>
2024-08-31 12:12:59 +0300

    added endpoints for employs

--- src/main/java/com/example/demo/controller/FirmController.java
+++ src/main/java/com/example/demo/controller/FirmController.java
@@ -1,11 +1,14 @@
 package com.example.demo.controller;
 
 import com.example.demo.model.Department;
+import com.example.demo.model.Employee;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class FirmController {
 
+
+    //  Departments
     @PostMapping("department/create")
     String createDepartment(@RequestBody Department department) {
         return "Create department";
@@ -24,7 +27,27 @@ public class FirmController {
     @DeleteMapping("department/delete/{id}")
     String deleteDepartment(@PathVariable int id) {
         return "deleteDepartment";
+    }
 
+    //  Employs
+    @PostMapping("employee/create")
+    String createEmployee(@RequestBody Employee employee) {
+        return "Create Employee";
+    }
+
+    @GetMapping("employee/read/{id}")
+    String readEmployee(@PathVariable int id) {
+        return "readEmployee";
+    }
+
+    @PutMapping("employee/update")
+    String updateEmployee(@RequestBody Employee employee) {
+        return "updateEmployee";
+    }
+
+    @DeleteMapping("employee/delete/{id}")
+    String deleteEmployee(@PathVariable int id) {
+        return "deleteEmployee";
     }
 
 }
